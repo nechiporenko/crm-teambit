63943a99dff092dc8e0a1d14dfeb7873886225a5
nechiporenko <s.nechiporenko@gmail.com>
2016-09-23 17:17:30 +0300

    First modal added

---
+++ README.md
@@ -0,0 +1,16 @@
+# crm-teambit
+
+CRM-teambit custom elements
+
+Getting Started:
+- install the dependencies
+1) bower install
+2) npm install
+
+- copy favicons, jquery & modernizr to /dist, build index.html:
+3) grunt init
+
+- do the things:
+4) grunt
+
+
---
+++ src/js/main.js
@@ -0,0 +1,100 @@
+﻿'use strict'
+
+var Modal = (function () {
+    var popUp = function (id) {
+        this.modalId = id;
+        this.overlayClass = 'b-modal__overlay';
+        this.overlayId = 'm_ovrl';
+        this.activeClass = 'active';
+        this.closeClass = 'b-modal__close';
+        this.modal = document.getElementById(this.modalId);
+        this.button = this.modal.getElementsByClassName(this.closeClass)[0].addEventListener('click', this.close.bind(this));
+    };
+
+
+    popUp.prototype.addOverlay = function () {
+        var overlay = document.createElement('div');
+        overlay.className = this.overlayClass;
+        overlay.setAttribute('id', this.overlayId);
+        document.body.appendChild(overlay);
+        overlay.addEventListener('click', this.close.bind(this));
+    };
+
+    popUp.prototype.center = function () {
+        var top, left,
+            page = document.body.parentElement,
+            style = getComputedStyle(this.modal);
+
+        top = Math.max(page.offsetHeight - outerHeightEl(this.modal), 0) / 2;
+        left = Math.max(page.offsetWidth - outerWidthEl(this.modal), 0) / 2;
+
+        this.modal.style.top = top + window.pageYOffset + 'px';
+        this.modal.style.left = left + window.pageXOffset + 'px';
+
+        function outerHeightEl(el) {
+            var height = el.offsetHeight;
+            height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+            return height;
+        };
+
+        function outerWidthEl(el) {
+            var width = el.offsetWidth;
+            width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+            return width;
+        };
+    };
+
+    popUp.prototype.open = function () {
+        this.addOverlay();
+        this.modal.style.visibility = 'hidden';
+        this.modal.style.display = 'block';
+        this.center();
+        window.addEventListener('resize', this.center.bind(this));
+        this.modal.style.visibility = 'visible';
+    };
+
+    popUp.prototype.close = function () {
+        var overlay = document.getElementById(this.overlayId);
+        overlay.removeEventListener('click', this.close);
+        overlay.parentElement.removeChild(overlay);
+        this.modal.style.display = 'none';
+        window.removeEventListener('resize', this.center);
+    };
+
+    return popUp;
+})();
+
+
+(function () {
+    var modal_btn = document.querySelectorAll('[data-modal]');
+    if (modal_btn.length > 0) {
+        var modal = {};
+        Array.prototype.forEach.call(modal_btn, function (el, i) {
+            var id = el.getAttribute('data-modal');
+            modal[i] = new Modal(id);
+            el.addEventListener('click', modal[i].open.bind(modal[i]));
+        });
+    }
+})();
+
+
+
+
+
+
+
+
+////helpers
+//function addClass(el, className) {
+//    if (el.classList)
+//        el.classList.add(className);
+//    else
+//        el.className += ' ' + className;
+//};
+
+//function removeClass(el, className) {
+//    if (el.classList)
+//        el.classList.remove(className);
+//    else
+//        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+//};
